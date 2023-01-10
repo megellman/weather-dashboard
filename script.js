aba4dc4a0ef022cf9dd2a436c16fc570e628e6c6
Megan Ellman <meganlellman@gmail.com>
2023-01-10 15:59:01 -0800

    removed json file

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('js connected')
+var key = 'ecbca053dcf966e18511cc24a49d0055';
+var city = '';
+var state = '';
+// var lat = ;
+// var lon = ;
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+
+// var url = `api.openweathermap.org/data/2.5/forecast?q=ozark&appid=${key}`;
+
+var form = $('#searchBar');
+var city = $('#city');
+
+
+var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
+
+var urlTwo = `https://api.openweathermap.org/data/2.5/forecast?lat=33.835289&lon=-117.914497&appid=${key}`;
+
+fetch(url)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    });
