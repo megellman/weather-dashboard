7c0531aad085c31df6817fe45d728875bc373ced
Megan Ellman <meganlellman@gmail.com>
2023-01-10 17:20:27 -0800

    created fetch request and added weather info to page

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 var key = 'ecbca053dcf966e18511cc24a49d0055';
-var city = '';
-var state = '';
-// var lat = ;
-// var lon = ;
 
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-// http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
-
-// var url = `api.openweathermap.org/data/2.5/forecast?q=ozark&appid=${key}`;
 
 var form = $('#searchBar');
 var city = $('#city');
 
 
-var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
+// var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
 
-var urlTwo = `https://api.openweathermap.org/data/2.5/forecast?lat=33.835289&lon=-117.914497&appid=${key}`;
 
-fetch(url)
-    .then(function (response){
+// var urlTwo = `https://api.openweathermap.org/data/2.5/forecast?lat=33.835289&lon=-117.914497&appid=${key}`;
+
+
+
+form.submit(function (event) {
+    event.preventDefault();
+
+    // var taco = document.createElement("button")
+    // taco.innerHTML = "taco time"
+    // document.querySelector("#searchBar").appendChild(taco)
+
+    console.log(city.val());
+    var urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city.val()}&units=imperial&appid=${key}`
+    var urlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city.val()}&units=imperial&appid=${key}`
+
+    fetch(urlWeather)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var weatherHeader = document.createElement('h2');
+            weatherHeader.innerHTML = data.name;
+            document.getElementById('weather-container').append(weatherHeader);
+            // var date = document.createElement('h3');
+            // date.innerHTML = data.dt;
+            // weatherHeader.append(date);
+            var temp = document.createElement('p');
+            temp.innerHTML = `Temp: ${data.main.temp} \u00B0F`;
+            weatherHeader.append(temp);
+            var wind = document.createElement('p');
+            wind.innerHTML = `Wind: ${data.wind.speed} MPH`;
+            weatherHeader.append(wind);
+            var humidity = document.createElement('p');
+            humidity.innerHTML = `Humidity: ${data.main.humidity} %`;
+            weatherHeader.append(humidity);
+        });
+    fetch(urlFiveDay)
+    .then(function (response) {
         return response.json();
     })
-    .then(function (data){
+    .then(function (data) {
         console.log(data);
+
     });
+})
+
+// for (let i = 0; i < array.length; i+=8) {
+//     const element = array[i];
+    
+// }
